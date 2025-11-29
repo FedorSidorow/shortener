821b99bf11bdbdbc79b9504263c2182787615d18
FedorSidorow <robokillhtc@gmail.com>
2025-11-29 22:49:03 +0300

    shit

--- internal/router/run.go
+++ internal/router/run.go
@@ -10,7 +10,7 @@ import (
 func Run() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /", handler.ShortThisURL)
-	mux.HandleFunc("GET /{key}", handler.GetFullURL)
+	mux.HandleFunc("/{key}", handler.GetFullURL)
 
 	err := http.ListenAndServe(":8080", mux)
 
