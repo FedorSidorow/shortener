f67d15bfc14ddf845d15295c55dbca617bb56b91
FedorSidorow <robokillhtc@gmail.com>
2026-04-04 18:53:45 +0300

    Флаги сборки и компиляции

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -14,7 +14,32 @@ import (
 	"github.com/FedorSidorow/shortener/internal/storage"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func printBuildInfo() {
+	version := buildVersion
+	if version == "" {
+		version = "N/A"
+	}
+	date := buildDate
+	if date == "" {
+		date = "N/A"
+	}
+	commit := buildCommit
+	if commit == "" {
+		commit = "N/A"
+	}
+	log.Printf("Build version: %s", version)
+	log.Printf("Build date: %s", date)
+	log.Printf("Build commit: %s", commit)
+}
+
 func main() {
+	printBuildInfo()
 	app, err := run()
 	if err != nil {
 		log.Printf("Error: %s\n", err)
